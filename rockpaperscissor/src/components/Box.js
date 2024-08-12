13f5008b78061fa3a7a9b64f26638e598d67781f
rslivepark <rslivepark@gmail.com>
2024-08-12 14:30:09 +0900

    4. 게임 로딩이미지 추가

--- rockpaperscissor/src/components/Box.js
+++ rockpaperscissor/src/components/Box.js
@@ -10,7 +10,11 @@ export default function Box({ title, item, result }) {
       <h1 className='box-title'>{title}</h1>
       <img
         className='item-img'
-        src={item && item.img}
+        src={
+          item && item.img
+            ? item.img
+            : 'https://img.freepik.com/premium-vector/hands-playing-rock-paper-scissors-game-flat-design-style-vector-illustration_540284-598.jpg'
+        }
         alt='rocksicssorspaper'
       />
       <h2>{result}</h2>
